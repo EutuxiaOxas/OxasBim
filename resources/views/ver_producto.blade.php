de6938d4b4697b3a30fa648fd2abf5b453cb9a89
Alexis Montilla <alexisamm9261@gmail.com>
2021-06-03 13:40:53 -0400

    Update ver_producto.blade.php

--- resources/views/ver_producto.blade.php
+++ resources/views/ver_producto.blade.php
@@ -27,8 +27,17 @@
 <div class="container mt-md-5" itemscope itemtype="https://schema.org/Product">
 	<div id="add_alert" style="display: none;" class="alert alert-success mt-3">Producto Agregado con éxito!</div>
 
-    <div class="row  d-md-none">
-        <h1 class="text-2xl color-secondary text-rubik font-semibold mt-2" itemprop="name"> {{ $product->title }} </h1>
+    <div class="row px-2 d-md-none">
+        <div class="col-auto ml-auto">
+            <span class="tag-available text-rubik">
+                <svg class="pb-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" width="20px" height="20px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm7 10c0 4.52-2.98 8.69-7 9.93-4.02-1.24-7-5.41-7-9.93V6.3l7-3.11 7 3.11V11zm-11.59.59L6 13l4 4 8-8-1.41-1.42L10 14.17z"/></svg>
+                Producto disponible
+            </span>
+        </div>
+        <h1 class="text-lg color-secondary text-rubik font-semibold mt-2" itemprop="name"> {{ $product->title }} </h1>
+        <div class="col-auto">
+            <div class="categorie_product_detail text-rubik">Categoria: <a class="text_no_decoration" href="{{route('product.category.show', $product->category->slug)}}"><strong class="text-secondary" itemprop="category">{{$product->category->title}}</strong></a></div>
+        </div>
         <div class="text-rubik mt-3 mb-4" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
             <span class="price_product_detail"> <span itemprop="priceCurrency">$</span> <span itemprop="price">{{number_format($product->price, 2)}}</span></span>
             <span class="price_product_detail_reference ml-2">${{number_format($product->price_reference, 2)}}</span>

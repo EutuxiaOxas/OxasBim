789caa632d25b4b4ee6cf5caa5291966265fa4fd
Alexis Montilla <alexisamm9261@gmail.com>
2021-06-03 13:29:50 -0400

    Update productos.blade.php

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -24,9 +24,9 @@
 			<div class="row">
 				<div class="col-12 my-3">
 					@if(isset($product_categorie))
-					<h1 class="text-rubik font-light">Productos en categoria: <small>{{$product_categorie->title}}</small></h1>
+					<h1 class="text-rubik font-light text-lg">Productos en categoria: <small>{{$product_categorie->title}}</small></h1>
 					@else
-					<h1 class="text-rubik font-light">Productos</h1>
+					<h1 class="text-rubik font-light text-lg">Productos</h1>
 					@endif
 
 					@if(isset($productos) && sizeof($productos) > 0)
